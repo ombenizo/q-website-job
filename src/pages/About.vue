b15ba66028221d5f99b7499ff33a6912721ffe64
ombenizo <ombeni.mwanga@gmail.com>
2022-03-30 12:37:35 +0300

    updated about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,58 +1,16 @@
 <template>
-  <q-layout view="lHr lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
-      </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/" label="Home" />
-      </q-tabs>
-    </q-header>
-<div class="q-ma-lg">
-<ul v-for="todo in todos" :key="todo.subject">
-  <li>
- {{todo.subject}}
-  </li>
-</ul>
-</div>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+  <div>
+    <h3>Lorem ipsum dolor sit.</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae excepturi doloribus incidunt, temporibus exercitationem nam, cum at ullam eum quidem animi alias sequi iure ratione? Rerum quo, quibusdam ducimus recusandae illum inventore dolores optio amet vitae, reiciendis sunt nisi atque vel expedita nobis. Corporis laboriosam libero laudantium fugit, earum deserunt?</p>
+  </div>
 </template>
 
 <script>
-import { ref } from "vue";
-
 export default {
-  setup() {
-    const leftDrawerOpen = ref(false);
-    const rightDrawerOpen = ref(false);
-    const todos = ref([{ subject: "Shoping" }, { subject: "Swiming" }]);
-    const newtodos = ref([]);
 
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-
-      rightDrawerOpen,
-      toggleRightDrawer() {
-        rightDrawerOpen.value = !rightDrawerOpen.value;
-      },
-    };
-  },
-};
+}
 </script>
+
+<style>
+
+</style>
