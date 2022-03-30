6ad4bbe99165d7f509463cc65f8fc0c97404a566
ombenizo <86510054+ombenizo@users.noreply.github.com>
2022-03-30 12:44:12 +0300

    Update About.vue
    update about page online on github

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,6 +3,15 @@
     <h3>Lorem ipsum dolor sit.</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae excepturi doloribus incidunt, temporibus exercitationem nam, cum at ullam eum quidem animi alias sequi iure ratione? Rerum quo, quibusdam ducimus recusandae illum inventore dolores optio amet vitae, reiciendis sunt nisi atque vel expedita nobis. Corporis laboriosam libero laudantium fugit, earum deserunt?</p>
   </div>
+   <div>
+    <h3>Lorem ipsum dolor sit.</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae excepturi doloribus incidunt, temporibus exercitationem nam, cum at ullam eum quidem animi alias sequi iure ratione? Rerum quo, quibusdam ducimus recusandae illum inventore dolores optio amet vitae, reiciendis sunt nisi atque vel expedita nobis. Corporis laboriosam libero laudantium fugit, earum deserunt?</p>
+  </div>
+   <div>
+    <h3>Lorem ipsum dolor sit.</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae excepturi doloribus incidunt, temporibus exercitationem nam, cum at ullam eum quidem animi alias sequi iure ratione? Rerum quo, quibusdam ducimus recusandae illum inventore dolores optio amet vitae, reiciendis sunt nisi atque vel expedita nobis. Corporis laboriosam libero laudantium fugit, earum deserunt?</p>
+  </div>
+  
 </template>
 
 <script>
